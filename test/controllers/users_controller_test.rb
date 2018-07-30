5412a91bb13d867ea3b5f8ce95d35695b69df0bc
VadimKuksa <kukssa00@gmail.com>
2018-07-30 15:21:46 +0700

    Finish user signup

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,7 +3,13 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get signup_path
-    assert_response :success
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
   end
 
 end
